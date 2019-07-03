ced2b4be9f828680bda3bd72cd2eb61ea095d4f2
Diego Fredes <diego.fredes@externos.bci.cl>
2019-07-02 20:37:10 -0400

    ya llega a pagina de tottus

--- features/step_definitions/google.js
+++ features/step_definitions/google.js
@@ -1,26 +1,43 @@
-const {Given, When, Then} = require('cucumber');
+const {Given, When, Then} = require('cucumber')
 //const Selector = require('testcafe').Selector;
-const {Selector, t} = require('testcafe');
-var googlePage = require('../support/pages/google-page.js');
+const {Selector, t} = require('testcafe')
+var googlePage = require('../support/pages/google-page.js')
 
 Given('I am open Google\'s search page', async function() {
-    await testController.navigateTo('https://google.com');
+    await testController.navigateTo('https://google.com')
 });
 
 When('I am typing my search request {string} on Google',  async function(text)  {
-    var input = Selector('.gLFyf').with({boundTestRun: testController});
-    await this.addScreenshotToReport();
-    await testController.typeText(input, text);
+    var input = Selector('.gLFyf').with({boundTestRun: testController})
+    await this.addScreenshotToReport()
+    await testController.typeText(input, text)
+    await testController.pressKey("enter")
 });
 
 Then('I press the {string} key on Google',  async function(text)  {
-    await testController.pressKey(text);
+   //implementar
 });
 
 Then('I click the Google\'s result {string}', async function (text) {
-   await googlePage.GooglePage(text,t)
+   //await googlePage.GooglePage(text,t)
+   var elemento=await Selector('span').withText(text)
+   clickSucursalIfExists(elemento)
+   await testController.wait(10000)
+   await testController.click(Selector('#new_tottus_nav_cliente_recetas').find('a'))
+   await testController.click(Selector('button').withText('ALTA').find('img'))
+   var input = Selector('#search').with({boundTestRun: testController})
+   await testController.typeText(input, 'corona')
+   await testController.click(Selector('h3').withText('CORONA DE ROLLITOS DE CANELA'))
 });  
 
+async function clickSucursalIfExists(element){
+    if(element.exists){
+       await testController.click(element)
+       await testController.click(Selector("#akp_uid_0").find("a"))
+    }else{
+        //TODO investigar como terminar el test
+    }
 
+}
 
 
